3943f0a108e0f7ee955880f760ae3a7a7af3ff9a
z3n1th21 <throwaway182930@gmail.com>
2022-05-18 15:16:21 -0400

    Slash commands.

--- commands/727.js
+++ commands/727.js
@@ -1,10 +1,10 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('727')
-		.setDescription('Replies with WYSI!'),
-	async execute(interaction) {
-		await interaction.reply('WYSI');
-	},
+    data: new SlashCommandBuilder()
+        .setName('727')
+        .setDescription('Replies with WYSI!'),
+    async execute(interaction) {
+        await interaction.reply('WYSI');
+    },
 };
--- deploy-commands.js
+++ deploy-commands.js
@@ -10,13 +10,20 @@ const commandsPath = path.join(__dirname, 'commands');
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
-	const filePath = path.join(commandsPath, file);
-	const command = require(filePath);
-	commands.push(command.data.toJSON());
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    commands.push(command.data.toJSON());
 }
 
 const rest = new REST({ version: '9' }).setToken(token);
 
-rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
-	.then(() => console.log('Successfully registered application commands.'))
-	.catch(console.error);
+if (process.argv[2] === 'push') {
+    rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+        .then(() => console.log('Successfully registered application commands globally.'))
+        .catch(console.error);
+}
+else {
+    rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+        .then(() => console.log('Successfully registered application commands to test server.'))
+        .catch(console.error);
+}
