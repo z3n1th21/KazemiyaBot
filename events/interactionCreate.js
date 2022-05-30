0d4753aae06b7788c171c273b8c10a8a15bcbe2e
z3n1th21 <throwaway182930@gmail.com>
2022-05-29 23:03:57 -0400

    Deploy

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,7 +1,7 @@
 module.exports = {
     name: 'interactionCreate',
     async execute(interaction) {
-        console.log(`${interaction.user.tag} in #${interaction.channel.name} triggered an interaction.`);
+        // console.log(`${interaction.user.tag} in #${interaction.channel.name} triggered an interaction.`);
 
         // Handle interaction, check if command and try to respond
         const client = interaction.client;
