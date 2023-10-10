8f3780dcddba555ccde4dc30cafd20300f60058a
szhang713 <szhang713@gatech.edu>
2023-10-10 02:20:09 -0400

    added chat args processing

--- src/event/message.js
+++ src/event/message.js
@@ -1,35 +1,45 @@
 const logger = require('../utility/logger.js');
+
 module.exports = {
     name: 'messageCreate',
     async execute(message) {
         const prefix = ')';
         const client = message.client;
 
+        if (!message.content.startsWith(prefix)) return;
         if (message.author.bot) return;
         if (!message.guild) return;
-        if (!message.content.startsWith(prefix)) return;
         if (!message.member) message.member = await message.guild.fetchMember(message);
 
-        // remove prefix, then split by space but keeping quoted strings intact, then remove quotes
-        const regex = /(?:[^\s"']+|("|')[^"']*("|'))+/g;
-        const args = message.content.slice(prefix.length).trim().toLowerCase().match(regex);
-        for (let i = 0; i < args.length; i++) {
-            args[i] = args[i].replaceAll(/'|"/g, '');
-        }
+        // parse content command and args
+        const regex = /(?:(([^\s"']+=("[^"]*"|'[^']*'))|[^\s"']+|("[^"]*"|'[^']*')))+/g;
+        // remove prefix, then split by space but keeping quoted strings intact
+        const array = message.content.slice(1).toLowerCase().match(regex);
 
-        // retrieve command
-        const command_name = args.shift();
+        const command_name = array.slice();
         if (command_name.length === 0) return;
         const command = client.commands.get(command_name);
+        if (!command) return;
         // if (!chat_command) chat_command = client.commands.get(client.aliases.get(command));
 
-        if (command) {
-            try {
-                command.chat(message, args, client);
-            } catch (error) {
-                logger.error(error);
-                await message.reply('there was an error executing this command');
+        // map each arg to their key, or their index if no key specified
+        const args = new Map();
+        array.forEach((arg, index) => {
+            arg.replaceAll(/"|'/g, '');
+            const i = arg.indexOf('=');
+            if (i >= 0) {
+                args.set(arg.substring(0, i), arg.substring(i + 1));
+            } else {
+                args.set(index, arg);
             }
+        });
+
+        try {
+            // may need to add `client` parameter later
+            command.chat(message, args);
+        } catch (error) {
+            logger.error(error);
+            await message.reply('there was an error executing this command');
         }
     },
 };
